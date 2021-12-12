b03ae1045a782ad9884bbbcc6ac16b69d2a94e08
keithwillis-bc <kwillis@beckman.com>
2021-12-11 23:30:01 -0500

    file upload, file load, file serve implemented

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,37 @@
-const express = require('express');
+const express = require("express");
+const multer = require("multer");
+const db = require("../data/database");
 
+const storageConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage: storageConfig });
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('profiles');
+router.get("/", async function (req, res) {
+  const users = await db.getDb().collection("users").find().toArray();
+  res.render("profiles", { users });
 });
 
-router.get('/new-user', function(req, res) {
-  res.render('new-user');
+router.get("/new-user", function (req, res) {
+  res.render("new-user");
+});
+
+router.post("/profiles", upload.single("userimage"), async (req, res) => {
+  const uploadedImageFile = req.file;
+  const userData = req.body;
+
+  await db.getDb().collection("users").insertOne({
+    name: userData.username,
+    imagePath: uploadedImageFile.path,
+  });
+
+  res.redirect("/");
 });
 
 module.exports = router;
